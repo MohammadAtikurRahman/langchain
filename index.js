76cb093e6ac920dee6cc032044314dedff948c56
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-09-07 14:44:57 +0600

    little work

--- index.js
+++ index.js
@@ -1,60 +1,80 @@
 import * as dotenv from "dotenv";
 dotenv.config();
+
 import { OpenAI } from "langchain/llms/openai";
+import { BufferMemory } from "langchain/memory";
+import { ConversationChain } from "langchain/chains";
 import { RetrievalQAChain, loadQAStuffChain } from "langchain/chains";
 import { CharacterTextSplitter } from "langchain/text_splitter";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { HNSWLib } from "langchain/vectorstores/hnswlib";
-
 import { CSVLoader } from "langchain/document_loaders/fs/csv";
 
-const loader = new CSVLoader("./office_dataset.csv");
-// ... your existing code ...
-
-
-const docs = await loader.load();
-
-// console.log(docs);
-
-const splitter = new CharacterTextSplitter({
-  chunkSize: 1536,
-  chunkOverlap: 200,
-});
-
-const allDocs = [];
-for (const doc of docs) {
-  const pageContent = doc.pageContent;
-  const user_name = "Md Niaj Shahriar Shishir";
-  const chunkHeader = `DOCUMENT NAME: ${user_name}\n\n---\n\n`;
-  const createdDocs = await splitter.createDocuments(
-    [pageContent],
-    [],
-    {
-      chunkHeader,
-      appendChunkOverlapHeader: true,
-    }
-  );
-  allDocs.push(...createdDocs);
+async function loadDocuments() {
+    const loader = new CSVLoader("./dataset.csv");
+    return await loader.load();
 }
 
-const vectorStore = await HNSWLib.fromDocuments(allDocs, new OpenAIEmbeddings());
+async function processDocuments(docs, user_name) {
+    const splitter = new CharacterTextSplitter({
+        chunkSize: 1536,
+        chunkOverlap: 200,
+    });
 
-const model = new OpenAI({ temperature: 0 });
+    const allDocs = [];
+    for (const doc of docs) {
+        const pageContent = doc.pageContent;
+        const chunkHeader = `DOCUMENT NAME: ${user_name}\n\n---\n\n`;
+        const createdDocs = await splitter.createDocuments([pageContent], [], {
+            chunkHeader,
+            appendChunkOverlapHeader: true,
+        });
+        allDocs.push(...createdDocs);
+    }
 
-const chain = new RetrievalQAChain({
-  combineDocumentsChain: loadQAStuffChain(model),
-  retriever: vectorStore.asRetriever(),
-  returnSourceDocuments: true,
-});
+    return allDocs;
+}
 
+async function getVectorStore(documents) {
+    return await HNSWLib.fromDocuments(documents, new OpenAIEmbeddings());
+}
 
+async function handleConversation(input, qaChain, conversationChain) {
+    const resFromQAChain = await qaChain.call({ query: input });
+    
+    if (resFromQAChain.text && resFromQAChain.text.trim() !== '') {
+        return resFromQAChain.text;
+    }
 
+    const resFromConversationChain = await conversationChain.call({ input: input });
+    if (resFromConversationChain.response && resFromConversationChain.response.text && resFromConversationChain.response.text.trim() !== '') {
+        return resFromConversationChain.response.text;
+    }
 
-const queryuser_name = "Md Shahriar Shishir";
-const queryField = "phone"; // or "sku", "brand", etc.
-const query = `What is the ${queryField} of ${queryuser_name}?`;
+    return undefined;
+}
 
-const res = await chain.call({ query });
-//console.log(JSON.stringify(res, null, 2));
+async function main() {
+    const docs = await loadDocuments();
+    const processedDocs = await processDocuments(docs, "1:8 Pagani Huayra Bc Bricks Assemble Car");
+    const vectorStore = await getVectorStore(processedDocs);
 
-console.log(res.text);
+    const qaModel = new OpenAI({ temperature: 0 });
+    const qaChain = new RetrievalQAChain({
+        combineDocumentsChain: loadQAStuffChain(qaModel),
+        retriever: vectorStore.asRetriever(),
+        returnSourceDocuments: true,
+    });
+
+    const conversationModel = new OpenAI({});
+    const memory = new BufferMemory();
+    const conversationChain = new ConversationChain({
+        llm: conversationModel,
+        memory: memory,
+    });
+
+    console.log(await handleConversation("What is the sku of 1:8 Pagani Huayra Bc Bricks Assemble Car?", qaChain, conversationChain));
+    console.log(await handleConversation("wha is price of 1:8 Pagani Huayra Bc Bricks Assemble Car", qaChain, conversationChain));
+}
+
+main();
