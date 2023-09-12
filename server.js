18992eed7f7d2df9001e74ea4e908002bd045dab
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-09-12 18:45:34 +0600

    done for

--- server.js
+++ server.js
@@ -23,8 +23,7 @@ import csv from 'csv-parser';
 const app = express();
 
 const loader1 = new CSVLoader("./datset/dataset.csv");
-const loader2 = new CSVLoader("./datset/delivery.csv");
-const loader3 = new CSVLoader("./datset/areaCode.csv");
+const loader2 = new CSVLoader("./datset/areaCode.csv");
 
 
 // ... Add more loaders for additional CSVs ...
@@ -71,14 +70,6 @@ fs.createReadStream(areaCodePath)
     console.log('CSV file processing finished.');
   });
 
-
-
-
-
-
-
-
-
 app.use(cors());
 app.use(express.json());
 
@@ -91,8 +82,8 @@ app.use(express.json());
 async function processDocuments(loader, user_name) {
   const docs = await loader.load();
   const splitter = new CharacterTextSplitter({
-    chunkSize: 1536,
-    chunkOverlap: 200,
+    chunkSize: 3036,
+    chunkOverlap: 2000,
   });
   const allDocs = [];
   for (const doc of docs) {
@@ -110,12 +101,11 @@ async function processDocuments(loader, user_name) {
 let qaChain, conversationChain;
 
 async function setupChains() {
-  const allDocs1 = await processDocuments(loader1, "User Name 1");
-  const allDocs2 = await processDocuments(loader2, "User Name 2");
+  const allDocs1 = await processDocuments(loader1, "dataset");
 
-  const allDocs3 = await processDocuments(loader3, "User Name 3");
+  const allDocs2 = await processDocuments(loader2, "areacode");
 
-  const allDocs = [...allDocs1, ...allDocs2, ...allDocs3];
+  const allDocs = [...allDocs1,...allDocs2];
 
   const vectorStore = await HNSWLib.fromDocuments(
     allDocs,
@@ -136,6 +126,8 @@ async function setupChains() {
     memory: memory,
   });
 }
+
+
 
 async function handleConversation(input) {
   const resFromQAChain = await qaChain.call({ query: input });
@@ -161,90 +153,84 @@ async function handleConversation(input) {
   return { text: "Unable to find an answer." };
 }
 
-//const chat = new ChatOpenAI();
 
-// Move these outside the endpoint to maintain a single, persistent instance across requests.
 const modelForDemo = new OpenAI({});
 const chatHistoryForDemo = new ChatMessageHistory();
 const memoryForDemo = new BufferMemory({ chatHistory: chatHistoryForDemo });
 
 
+
+function augmentMessageWithInstructions(originalMessage) {
+  
+  if (originalMessage.includes("price")&& !originalMessage.includes("previous")) {
+    return originalMessage + "and this product name also Give a short answer with just the weight";
+  }
+
+  // if (/\d+/.test(originalMessage)) {
+  //   const matchedArea = areaCode.find(d => d.area_code === originalMessage);
+  //   originalMessage = matchedArea;
+  //   console.log("inside the areacode",originalMessage.area_orginal)
+  //   return originalMessage.area_orginal + "";
+    
+  // }
+
+
+  // const matchedArea = areaCode.find(d => d.area_orginal === originalMessage);
+  // const matcharea_original = matchedArea.area_orginal;
+  // if (originalMessage.includes(matcharea_original)) {
+
+  //   originalMessage = matcharea_original;
+  //   return originalMessage + " is area_orginal. area_original has areacode_charge row wise .. this areacode_charge added with previous product price";
+  // }
+
+
+
+  
+  return originalMessage; // Return the original message if no conditions matched
+}
+
+function addCustomTextToResponse(datasetResponse, demoResponse) {
+  let customText = "";
+
+  if (datasetResponse.includes("price")) {
+    customText += "What is your location ";
+  }
+
+  if (demoResponse.includes("price")) {
+    customText += "or Area Code or Area ?";
+  }
+
+  return customText;
+}
+
+
+
 app.post("/api/", async (req, res) => {
   let message = req.body.message;
-   
 
+  message = augmentMessageWithInstructions(message);
 
-  let promptPrice = "and please answer it with it's weight very shortly";
-  if (message.includes("price")) {
-    message = message + " " + promptPrice;
-    console.log("prompt", message);
-  } 
-
-
-
-   
-  const locationExists = deliveryData.some((item) => message.includes(item.location));
-   let promptDelivery = " this location for rules,weight-dl,operator,deliveryPrice and location and previous weight-dl based delivery charge and please dont show others all comparsion";
-
-  if (locationExists) {
-
-      message = message + " " + promptDelivery;
-
-    // console.log("Message contains a valid location from deliveryData.");
-    // console.log(locationExists);
-    // console.log("prompt", message)
-  } 
-
-
-
-  let promptArea = " it has areacode_charge and areacode_charge can be 0 or 39 and it show please and this is 0 or 39 and please tell one area_orginal wise area_charge and this charge please added previous totall price"
-  const matchedArea = areaCode.find(d => d.area_code === message);
-
-  if (matchedArea) {
-      console.log(matchedArea.area_orginal);
-      const area_with_charge = matchedArea.area_orginal;
-
-
-      message = area_with_charge + " " + promptArea;
-
-      console.log(message)
-
-
-  }
-  
-
-
-
-
-  // let promptDelivery = "and if you get region,code,area then this wise you will get a charge and charge can 39,79 (39+79) or 0,79 (0+79) or 39,0 (39+0) or 0,0 (0+0) of addition added previous final price";
-  // if(message.includes("code")) {
-  //   message = message + " " + promptDelivery;
-  //   console.log("prompt", message);
-  // } 
-   
-  
-
-
-
-
-
+  console.log("prompt message",message);
 
   if (message) {
     const response = await handleConversation(message);
 
-    // Use the persistent demoChain for conversation context.
     const demoChain = new ConversationChain({ llm: modelForDemo, memory: memoryForDemo });
     const demoResponse = await demoChain.call({ input: message });
-    console.log("Demo Chain Response:", demoResponse);
+  //  console.log("Demo Chain Response:", demoResponse);
 
     const dataset_response = response;
 
 
-    console.log("Demo Dataset Response:", dataset_response.text);
+  //  console.log("Demo Dataset Response:", dataset_response.text);
 
     const final_result= dataset_response.text;
 
-    res.json({ botResponse: "\n\n" + "Dataset:" +final_result + "\n\n" + "System:" +demoResponse.response });
+
+    const customText = addCustomTextToResponse(final_result, demoResponse.response);
+
+
+    res.json({ botResponse: "\n\n" + "Dataset:" +final_result + "\n\n" + "System:" +demoResponse.response + "\n\n"+ customText });
     return;
   }
 
