cd19806a24587f6f67b21669bf0b668f9660f968
Md. Atikur Rahman <contact.atikurrahman@gmail.com>
2023-09-13 13:40:30 +0600

    at

--- server.js
+++ server.js
@@ -13,50 +13,41 @@ import { HNSWLib } from "langchain/vectorstores/hnswlib";
 import { CSVLoader } from "langchain/document_loaders/fs/csv";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { HumanMessage, SystemMessage } from "langchain/schema";
-// Import the required modules using ES6 module syntax
-import fs from 'fs';
-import csv from 'csv-parser';
-
-// Your code here
+import fs from "fs";
+import csv from "csv-parser";
 
 
 const app = express();
-
 const loader1 = new CSVLoader("./datset/dataset.csv");
 const loader2 = new CSVLoader("./datset/areaCode.csv");
+const loader3 = new CSVLoader("./datset/delivery.csv");
 
 
-// ... Add more loaders for additional CSVs ...
-
-let deliveryData = []; // To store the data as a global JSON variable
-
+let deliveryData = []; 
 const deliveryFilePath = "./datset/delivery.csv";
 
 fs.createReadStream(deliveryFilePath)
   .pipe(csv())
-  .on('data', (row) => {
+  .on("data", (row) => {
     // Store each row in the deliveryData array as JSON
     deliveryData.push({
       location: row.location,
       rules: row.rules,
-      weight_dl: row['weight-dl'],
+      weight_dl: row["weight-dl"],
       operator: row.operator,
       deliveryPrice: row.deliveryPrice,
     });
   })
-  .on('end', () => {
-    console.log('CSV file processing finished.');
+  .on("end", () => {
+    console.log("CSV file processing finished.");
   });
 
-
-
-  let areaCode = [];
-  const areaCodePath = "./datset/areaCode.csv"
-
+let areaCode = [];
+const areaCodePath = "./datset/areaCode.csv";
 
 fs.createReadStream(areaCodePath)
   .pipe(csv())
-  .on('data', (row) => {
+  .on("data", (row) => {
     // Store each row in the deliveryData array as JSON
     areaCode.push({
       delivery: row.delivery,
@@ -66,18 +57,12 @@ fs.createReadStream(areaCodePath)
       areacode_charge: row.areacode_charge,
     });
   })
-  .on('end', () => {
-    console.log('CSV file processing finished.');
+  .on("end", () => {
+    console.log("CSV file processing finished.");
   });
 
 app.use(cors());
 app.use(express.json());
-
-
-
-
-
-
 
 async function processDocuments(loader, user_name) {
   const docs = await loader.load();
@@ -105,7 +90,9 @@ async function setupChains() {
 
   const allDocs2 = await processDocuments(loader2, "areacode");
 
-  const allDocs = [...allDocs1,...allDocs2];
+  const allDocs3 = await processDocuments(loader3, "delivery-method");
+
+  const allDocs = [...allDocs1, ...allDocs2, ...allDocs3];
 
   const vectorStore = await HNSWLib.fromDocuments(
     allDocs,
@@ -127,118 +114,130 @@ async function setupChains() {
   });
 }
 
-
-
 async function handleConversation(input) {
   const resFromQAChain = await qaChain.call({ query: input });
 
   if (resFromQAChain.text && resFromQAChain.text.trim() !== "") {
-    chatHistoryForDemo.addUserMessage(input);  // Save user input to chat history
-    chatHistoryForDemo.addAIChatMessage(resFromQAChain.text);  // Save AI response to chat history
+    chatHistoryForDemo.addUserMessage(input); // Save user input to chat history
+    chatHistoryForDemo.addAIChatMessage(resFromQAChain.text); // Save AI response to chat history
     return resFromQAChain;
   }
 
-  const resFromConversationChain = await conversationChain.call({ input: input });
+  const resFromConversationChain = await conversationChain.call({
+    input: input,
+  });
 
   if (
     resFromConversationChain.response &&
     resFromConversationChain.response.text &&
     resFromConversationChain.response.text.trim() !== ""
   ) {
-    chatHistoryForDemo.addUserMessage(input);  // Save user input to chat history
-    chatHistoryForDemo.addAIChatMessage(resFromConversationChain.response.text);  // Save AI response to chat history
+    chatHistoryForDemo.addUserMessage(input); // Save user input to chat history
+    chatHistoryForDemo.addAIChatMessage(resFromConversationChain.response.text); // Save AI response to chat history
     return resFromConversationChain;
   }
 
   return { text: "Unable to find an answer." };
 }
 
-
 const modelForDemo = new OpenAI({});
 const chatHistoryForDemo = new ChatMessageHistory();
 const memoryForDemo = new BufferMemory({ chatHistory: chatHistoryForDemo });
 
-
-
 function augmentMessageWithInstructions(originalMessage) {
-  
-  if (originalMessage.includes("price")&& !originalMessage.includes("previous")) {
-    return originalMessage + "and this product name also Give a short answer with just the weight";
+  if (
+    originalMessage.includes("price") &&
+    !originalMessage.includes("previous")
+  ) {
+    return (
+      originalMessage +
+      "and this product name also Give a short answer with just the weight"
+    );
   }
 
-  // if (/\d+/.test(originalMessage)) {
-  //   const matchedArea = areaCode.find(d => d.area_code === originalMessage);
-  //   originalMessage = matchedArea;
-  //   console.log("inside the areacode",originalMessage.area_orginal)
-  //   return originalMessage.area_orginal + "";
-    
-  // }
+  if (/\d+/.test(originalMessage)) {
+    const matchedArea = areaCode.find((d) => d.area_code === originalMessage);
+    originalMessage = matchedArea;
+    console.log("inside the areacode", originalMessage?.area_orginal);
+    return (
+      "what " +
+      originalMessage?.area_orginal +
+      " areacode_charge? and this areacode_charge added with previous product's price"
+    );
+  }
 
+  const matchedArea = areaCode.find((d) => d.area_orginal === originalMessage);
+  const matcharea_original = matchedArea.area_orginal;
+  if (originalMessage.includes(matcharea_original)) {
+    originalMessage = matcharea_original;
+    return (
+      "what " +
+      originalMessage +
+      " areacode_charge? and this areacode_charge added with previous product's price"
+    );
+  }
 
-  // const matchedArea = areaCode.find(d => d.area_orginal === originalMessage);
-  // const matcharea_original = matchedArea.area_orginal;
-  // if (originalMessage.includes(matcharea_original)) {
-
-  //   originalMessage = matcharea_original;
-  //   return originalMessage + " is area_orginal. area_original has areacode_charge row wise .. this areacode_charge added with previous product price";
-  // }
-
-
-
-  
-  return originalMessage; // Return the original message if no conditions matched
+  return originalMessage; //
 }
 
 function addCustomTextToResponse(datasetResponse, demoResponse) {
   let customText = "";
 
-  if (datasetResponse.includes("price")) {
-    customText += "What is your location ";
+  if (datasetResponse.includes("price") && !datasetResponse.includes("total")) {
+    customText += "What is your location ?";
   }
 
-  if (demoResponse.includes("price")) {
-    customText += "or Area Code or Area ?";
+  if (demoResponse.includes("total price")) {
+    customText += "any question ?";
   }
 
   return customText;
 }
-
-
 
 app.post("/api/", async (req, res) => {
   let message = req.body.message;
 
   message = augmentMessageWithInstructions(message);
 
-  console.log("prompt message",message);
+  console.log("prompt message", message);
 
   if (message) {
     const response = await handleConversation(message);
 
-    const demoChain = new ConversationChain({ llm: modelForDemo, memory: memoryForDemo });
+    const demoChain = new ConversationChain({
+      llm: modelForDemo,
+      memory: memoryForDemo,
+    });
     const demoResponse = await demoChain.call({ input: message });
-  //  console.log("Demo Chain Response:", demoResponse);
+    //  console.log("Demo Chain Response:", demoResponse);
 
     const dataset_response = response;
 
+    //  console.log("Demo Dataset Response:", dataset_response.text);
 
-  //  console.log("Demo Dataset Response:", dataset_response.text);
+    const final_result = dataset_response.text;
 
-    const final_result= dataset_response.text;
+    const customText = addCustomTextToResponse(
+      final_result,
+      demoResponse.response
+    );
 
-
-    const customText = addCustomTextToResponse(final_result, demoResponse.response);
-
-
-    res.json({ botResponse: "\n\n" + "Dataset:" +final_result + "\n\n" + "System:" +demoResponse.response + "\n\n"+ customText });
+    res.json({
+      botResponse:
+        "\n\n" +
+        "Dataset:" +
+        final_result +
+        "\n\n" +
+        "System:" +
+        demoResponse.response +
+        "\n\n" +
+        customText,
+    });
     return;
   }
 
   res.status(400).send({ error: "Message is required!" });
 });
-
-
-
 
 const PORT = 5000;
 setupChains()
